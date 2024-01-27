cf797f43db49a919454f13d4d930e7387b05d141
RAIBOTICS <85551218+raibotics1@users.noreply.github.com>
2024-01-28 00:29:05 +0300

    Update display.launch.py
    Переписан код

--- KMU_robot_description/launch/display.launch.py
+++ KMU_robot_description/launch/display.launch.py
@@ -1,64 +1,54 @@
-from launch_ros.actions import Node
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition
-import xacro
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
 import os
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('KMU_robot_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'KMU_robot.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_urdf = robot_description_config.toxml()
+    urdf_file_name = 'KMU_robot.xacro'
+    urdf = os.path.join(
+        get_package_share_directory('KMU_robot_description'),
+        'urdf',
+        urdf_file_name)
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
 
-    rviz_config_file = os.path.join(share_dir, 'config', 'display.rviz')
 
-    gui_arg = DeclareLaunchArgument(
-        name='gui',
-        default_value='True'
-    )
-
-    show_gui = LaunchConfiguration('gui')
-
-    robot_state_publisher_node = Node(
+    robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        parameters=[
-            {'robot_description': robot_urdf}
-        ]
+        output='screen',
+        parameters=[{'robot_description': robot_desc}],
+        arguments=[urdf]
     )
-
-    joint_state_publisher_node = Node(
-        condition=UnlessCondition(show_gui),
+    joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        name='joint_state_publisher'
+        name='joint_state_publisher',
+        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-
-    joint_state_publisher_gui_node = Node(
-        condition=IfCondition(show_gui),
+    joint_state_publisher_gui_node = launch_ros.actions.Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui'
+        name='joint_state_publisher_gui',
+        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
-
-    rviz_node = Node(
+    rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', rviz_config_file],
-        output='screen'
+        output='screen',
     )
 
-    return LaunchDescription([
-        gui_arg,
-        robot_state_publisher_node,
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+                                             description='Flag to enable joint_state_publisher_gui'),
+
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
+        robot_state_publisher_node,
         rviz_node
     ])
